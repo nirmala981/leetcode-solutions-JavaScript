d0adc12cf328a0400c84b8e9e4e664c08dede22f
VANA NIRMALA <nirmalavana822@gmail.com>
2026-08-10 18:28:03 +0530

    Time: 52 ms (13.34%), Space: 54.1 MB (12.51%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return{
+        toBe:function(otherval){
+            if (val===otherval){
+                return true
+        
+            }
+            throw new Error("Not Equal")
+            
+        },
+        notToBe:function(otherval){
+            if (val !==otherval){
+                return true
+            
+            }
+            throw new Error("Equal")
+        }
+    }
+    
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
