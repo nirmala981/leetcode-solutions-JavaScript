f603d5875d033687f71161b0c52a07c5c7814c4a
VANA NIRMALA <nirmalavana822@gmail.com>
2026-08-14 19:35:01 +0530

    Time: 45 ms (50.26%), Space: 53.9 MB (34.31%) - LeetHub

---
+++ 2666-allow-one-function-call/2666-allow-one-function-call.js
@@ -0,0 +1,12 @@
+var once = function(fn) {
+    let called = false;
+    let result;
+
+    return function(...args) {
+        if (!called) {
+            called = true;
+            result = fn(...args);
+            return result;
+        }
+    };
+};
