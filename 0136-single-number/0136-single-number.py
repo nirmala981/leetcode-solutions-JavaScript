208cd7ec57f65d99798147174e2d973e18e53494
VANA NIRMALA <nirmalavana822@gmail.com>
2026-08-16 16:06:33 +0530

    Time: 14 ms (21.12%), Space: 14.7 MB (8.11%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count={}
+        for num in nums:
+            if num in count:
+                count[num]+=1
+            else:
+                count[num]=1
+        
+        for num in count:
+            if count[num]==1:
+                return num
+        
