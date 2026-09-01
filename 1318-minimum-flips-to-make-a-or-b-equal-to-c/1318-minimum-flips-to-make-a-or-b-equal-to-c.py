a4d84f76501e7ae63cb956b2a1acfa055f7ced36
VANA NIRMALA <nirmalavana822@gmail.com>
2026-09-01 20:56:53 +0530

    Time: 21 ms (10.84%), Space: 12.3 MB (50.6%) - LeetHub

---
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def minFlips(self, a, b, c):
+        flips = 0
+
+        for i in range(32):
+            bit_a = (a >> i) & 1
+            bit_b = (b >> i) & 1
+            bit_c = (c >> i) & 1
+
+            if bit_c == 0:
+                flips += bit_a + bit_b
+            else:
+                if bit_a == 0 and bit_b == 0:
+                    flips += 1
+
+        return flips
